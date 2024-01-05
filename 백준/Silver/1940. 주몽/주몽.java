1dcd7419244b77803b6747980ba5e60ffed70264
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-05 14:23:39 +0900

    [Silver IV] Title: 주몽, Time: 396 ms, Memory: 17196 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String args[]) throws IOException{	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int arr[] = new int[N];
+        for(int i=0; i<N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int cnt = 0;
+        for(int i=0; i<N; i++) {
+        	for(int j=i+1; j<N; j++) {
+        		if(arr[i] + arr[j] == M) {
+        			cnt ++;
+        		}
+        	}
+        }
+        System.out.println(cnt);
+	}
+}
