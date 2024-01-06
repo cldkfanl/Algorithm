a61893f72fc4dd4364bb21345673276bcb9418ef
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-06 15:02:54 +0900

    [Silver IV] Title: 상근이의 여행, Time: 376 ms, Memory: 45564 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String args[]) throws IOException{	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int N =Integer.parseInt(st.nextToken());
+        	int M = Integer.parseInt(st.nextToken());
+        	for(int j=0; j<M; j++) {
+        		st = new StringTokenizer(br.readLine());
+        		int a = Integer.parseInt(st.nextToken());
+        		int b = Integer.parseInt(st.nextToken());
+        	}
+        	System.out.println(N-1);
+        }
+	}
+}
