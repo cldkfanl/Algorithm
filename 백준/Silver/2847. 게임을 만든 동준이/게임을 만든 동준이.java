250cc849048f8f205c94748d2df515f0bcc84823
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-06 14:36:51 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+        for(int i=0; i<N; i++) {
+        	arr[i] = Integer.parseInt(br.readLine());
+        }
+        int cnt = 0;
+        if(N >= 2) {
+            for(int i=N-2; i>=0; i--) {
+            	if(arr[i] >= arr[i+1]) {
+            		cnt += arr[i] - (arr[i+1]-1);
+            		arr[i] = arr[i+1]-1;
+            	}
+            }
+            System.out.println(cnt);
+        }
+        else {
+        	System.out.println(0);
+        }
+    }
+}
