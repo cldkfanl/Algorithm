2cf5fd55d1d53c8910a570cb60812906d0f0d6f4
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-06 14:14:15 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,13 +1,13 @@
-from collections import deque                      //Deque 사용
+from collections import deque
 
 N = int(input())
-deq = deque(enumerate(map(int, input().split())))  //입력 순서와 입력값을 deque에 저장
+deq = deque(enumerate(map(int, input().split())))
 arr = []
-while deq :                                        //deq가 비기 전까지
-    fir, sec = deq.popleft()                       //맨 앞에 있는 값 뽑아서
-    arr.append(fir + 1)                            //입력 순서는 arr에 저장
-    if sec > 0 :                                   //입력값이 0보다 크다면
-        deq.rotate(-sec + 1)                       //우측으로 이동, 본인을 제외해야 하기 때문에 + 1
-    elif sec < 0 :                                 //입력값이 0보다 작다면
-        deq.rotate(-sec)                           //좌측으로 이동.
-print(" ".join(map(str, arr)))                     //저장한 arr을 출력
+while deq :
+    fir, sec = deq.popleft()
+    arr.append(fir + 1)
+    if sec > 0 :
+        deq.rotate(-sec + 1)
+    elif sec < 0 :
+        deq.rotate(-sec)
+print(" ".join(map(str, arr)))
