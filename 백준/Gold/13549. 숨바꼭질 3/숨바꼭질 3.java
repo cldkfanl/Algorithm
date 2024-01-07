60b048d18be6ad8ba4ed5caad886b9c6568f53a2
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-07 14:58:35 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 164 ms, Memory: 18592 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, K;
+	static int arr[];
+	static int output = 0;
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[100001];
+        bfs(N);
+        System.out.println(output);
+	}
+	static void bfs(int num) {
+		 Queue<Integer> queue = new LinkedList<Integer>();
+		 queue.add(num);
+		 int index = num;
+		 arr[index] = 1;
+		 while(queue.isEmpty() == false) {
+			 int n = queue.remove();
+			 if(n == K) {
+				 output = arr[n]-1;
+			 }
+			 if(n*2<=100000 && (arr[n] < arr[n*2]||arr[n*2] == 0)) {
+				 arr[n*2] = arr[n];
+				 queue.add(n*2);
+			 }
+			 if(n-1>=0 && arr[n-1] == 0) {
+				 arr[n-1] = arr[n] + 1;
+				 queue.add(n-1);
+			 }
+			 if(n+1<=100000 && arr[n+1] == 0) {
+				 arr[n+1] = arr[n] + 1;
+				 queue.add(n+1);
+			 }
+		 }
+	}
+}
