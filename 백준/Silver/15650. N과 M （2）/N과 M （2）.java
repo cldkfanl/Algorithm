b7c60d9f55096635992875389e376080fbd02f81
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-07 15:03:09 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+	public static int arr[];
+	public static StringBuilder sb = new StringBuilder();
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M= Integer.parseInt(st.nextToken());
+        
+        
+
+        
+        arr = new int[M];
+        NandM(N,M,1,0);
+        System.out.println(sb);
+	}
+    public static void NandM(int N, int M, int start ,int depth) {
+    	if(depth == M) {
+    		for(int val : arr) {
+    			sb.append(val).append(' ');
+    		}
+    		sb.append('\n');
+    		return;
+    	}
+    	for(int i= start; i<=N; i++) {
+    		arr[depth] = i;
+    		NandM(N,M,i+1, depth+1);
+    	}
+    }
+}
