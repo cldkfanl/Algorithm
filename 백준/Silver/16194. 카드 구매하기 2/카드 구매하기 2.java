be66e7e4678d34bc62668987a363254cd3c3933c
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-08 09:13:16 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 144 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+    	int[] arr = new int[a+1];
+    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=1; i<=a; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[a+1];
+        for(int i=1; i<=a; i++) {
+        	dp[i] = arr[i];
+        	for(int j=1; j<=i; j++) {
+        		dp[i] = Math.min(arr[j] + dp[i-j], dp[i]);
+        	}
+        }
+        System.out.println(dp[a]);
+    }
+}
