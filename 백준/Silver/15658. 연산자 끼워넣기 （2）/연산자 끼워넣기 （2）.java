5e5121e356d327b2c83112882d21f31a5bcee74c
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-08 09:04:44 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 168 ms, Memory: 16392 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int arr[];
+	static int arr2[];
+	static int arr3[];
+	static int sum;
+	static long max = Integer.MIN_VALUE;
+	static long min = Integer.MAX_VALUE;
+	public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        arr2 = new int[4];
+        arr3 = new int[N-1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        sum = arr[0];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<4; i++) {
+        	arr2[i] = Integer.parseInt(st.nextToken());
+        }
+        arr2case(0);
+        System.out.println(max);
+        System.out.println(min);
+	}
+	static void arr2case(int depth) {
+		if(depth == N-1) {
+			for(int i=0; i<N-1; i++) {
+				calcul(i, arr3[i]);
+			}
+			max = Math.max(max, sum);
+			min = Math.min(min, sum);
+			sum = arr[0];
+			return;
+		}
+		for(int i=0; i<4; i++) {
+			if(arr2[i] > 0) {
+				arr2[i] --;
+				arr3[depth] = i;
+				arr2case(depth+1);
+				arr2[i] ++;
+			}
+		}
+	}
+	static void calcul(int count, int i) {
+		switch(i) {
+		case 0 : sum += arr[count+1]; break;
+		case 1 : sum -= arr[count+1]; break;
+		case 2 : sum *= arr[count+1]; break;
+		case 3 : sum /= arr[count+1]; break;
+		}
+	}
+}
