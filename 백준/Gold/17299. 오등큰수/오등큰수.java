bc5f308248807fc642ae3cd0096d6cdd3d183d7c
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-08 09:31:43 +0900

    [Gold III] Title: 오등큰수, Time: 996 ms, Memory: 143460 KB -BaekjoonHub

---
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int count;
+	public static int arr[];
+	public static int arr_2[];
+	public static int arr_count[] = new int[1000001];
+	public static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        count = Integer.parseInt(br.readLine());
+    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    	arr = new int[count];
+    	arr_2 = new int[count];
+        for(int i=0; i<count; i++) {
+        	int input = Integer.parseInt(st.nextToken());
+        	arr_count[input]++;
+        	arr[i] = input;
+        }
+        
+        asdf();
+        System.out.println(sb);
+	}
+	public static void asdf() {
+		Stack<Integer> stack = new Stack();
+		for(int i=0; i<count; i++) {
+			while (!stack.isEmpty() && arr_count[arr[stack.peek()]] < arr_count[arr[i]]) {
+				arr_2[stack.pop()] = arr[i];
+			}
+			stack.push(i);
+		}
+		while(!stack.isEmpty()) {
+			arr_2[stack.pop()] = -1;
+		}
+		for(int i=0; i<count; i++) {
+			sb.append(arr_2[i]).append(' ');
+		}
+	}
+}
