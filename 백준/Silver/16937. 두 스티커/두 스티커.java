2dfdcdc4a044a74f54d25485c06468f484905768
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-01-08 09:23:17 +0900

    [Silver III] Title: 두 스티커, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/16937. 두 스티커/두 스티커.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int arr[][];
+	static int N, H, W;
+	static int sum = 0;
+	static int max = 0;
+	public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	H = Integer.parseInt(st.nextToken());
+    	W = Integer.parseInt(st.nextToken());
+    	N = Integer.parseInt(br.readLine());
+    	arr = new int[N][2];
+    	for(int i=0; i<N; i++) {
+    		st = new StringTokenizer(br.readLine());
+    		arr[i][0] = Integer.parseInt(st.nextToken());
+    		arr[i][1] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	for(int i=0; i<N-1; i++) {
+        	dfs(i);
+    	}
+    	System.out.println(max);
+	}
+	static void dfs(int i) {
+		for(int j=i+1; j<N; j++) {
+			if(arr[i][0] + arr[j][0] <= H && Math.max(arr[i][1], arr[j][1]) <= W ||
+					arr[i][0] + arr[j][0] <= W && Math.max(arr[i][1], arr[j][1]) <= H) {
+				sum = arr[i][0] * arr[i][1] + arr[j][0] * arr[j][1];
+			}
+			else if(arr[i][0] + arr[j][1] <= H && Math.max(arr[i][1], arr[j][0]) <= W ||
+					arr[i][0] + arr[j][1] <= W && Math.max(arr[i][1], arr[j][0]) <= H) {
+				sum = arr[i][0] * arr[i][1] + arr[j][0] * arr[j][1];
+			}
+			else if(arr[i][1] + arr[j][0] <= H && Math.max(arr[i][0], arr[j][1]) <= W ||
+					arr[i][1] + arr[j][0] <= W && Math.max(arr[i][0], arr[j][1]) <= H) {
+				sum = arr[i][0] * arr[i][1] + arr[j][0] * arr[j][1];
+			}
+			else if(arr[i][1] + arr[j][1] <= H && Math.max(arr[i][0], arr[j][0]) <= W ||
+					arr[i][1] + arr[j][1] <= W && Math.max(arr[i][0], arr[j][0]) <= H) {
+				sum = arr[i][0] * arr[i][1] + arr[j][0] * arr[j][1];
+			}
+			if(max < sum) max = sum;
+		}
+	}
+}
