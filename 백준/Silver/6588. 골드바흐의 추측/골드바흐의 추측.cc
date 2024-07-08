eb89a8152f92d904e75027e6ee8aee9113a0d509
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-07-08 19:47:37 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 76 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -1,39 +1,33 @@
-#include <iostream>
 #include <vector>
+#include<iostream>
 using namespace std;
 
-vector<bool> prime(1000001,true);
-
-void era()
-{
-	for (long long i = 2; i*i <= 1000000; i++)
-	{
-		for (long long j = i * i; j <= 1000000; j += i)
-		{
-			prime[j] = false;
-		}
-	}
-}
 
 int main()
 {
-	era();
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	while (1)
-	{
-		long long n = 0;
-		cin >> n;
-		if (n == 0)
-			break;
-		for (long long i = 2; i <= n; i++)
-		{
-			if ((prime[i] == true) && (prime[n - i] == true))
-			{
-				cout << n << " = " << i << " + " << n - i <<'\n';
-				break;
-			}
-		}
-	}
+    vector<bool> arr(1000001, true);
+    for (int i = 2; i*i <= 1000000; i++) {
+        for (int j = i * i; j <= 1000000; j += i) {
+            arr[j] = false;
+        }
+    }
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    while (true) {
+        int n;
+        cin >> n;
+        if (n == 0) {
+            break;
+        }
+        for (int i = 3; i <= n; i+=2) {
+            if (arr[i] && arr[n - i]) {
+                cout << n << " = " << i << " + " << n - i << '\n';
+                break;
+            }
+        }
+    }
+
+    return 0;
 }
