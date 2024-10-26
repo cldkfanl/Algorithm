9547a0bc108e033a5a2f03b4cf28eb4aff71fa05
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2024-10-27 02:12:02 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    double area;
+
+    cin >> area;
+
+    printf("%.8f", sqrt(area) * 4);
+
+    return 0;
+}
