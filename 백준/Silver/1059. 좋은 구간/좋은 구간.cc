c170aedbd2ce880b339113fe93f0508e972fbe0e
JIung Choi <105538572+cldkfanl@users.noreply.github.com>
2025-06-08 02:23:33 +0900

    [Silver IV] Title: 좋은 구간, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int L, n;
+
+    cin >> L;
+
+    std::vector<int> vec_s;
+
+    int tmp, start = 0, end = 0;
+    for (int i = 0; i < L; i++) {
+        cin >> tmp;
+        vec_s.push_back(tmp);
+    }
+    sort(vec_s.begin(), vec_s.end());
+    cin >> n;
+
+    for (int i = 0; i < L; i++) {
+        if (vec_s[i] == n) {
+            cout << 0;
+            return 0;
+        }
+    }
+    for(int i=0; i<L; i++){
+        if (vec_s[i] > n) {
+            end = vec_s[i] - 1;
+            if (i == 0) start = 1;
+            else start = vec_s[i - 1] + 1;
+            break;
+        }
+    }
+    if (start > n || end < n) {
+        cout << 0;
+    }
+    else {
+        int result = (n - start) * (end - n + 1) + (end - n);
+        cout << result;
+    }
+
+
+    return 0;
+}
